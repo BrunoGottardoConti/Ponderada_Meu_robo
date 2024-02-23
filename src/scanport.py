0284b75a8b941c21921157519c91ff11bcc876be
BrunoGottardoConti <bruno.conti@sou.inteli.edu.br>
2024-02-23 07:43:44 -0300

    estudo

---
+++ src/scanport.py
@@ -0,0 +1,9 @@
+from serial.tools import list_ports
+import inquirer
+
+def scanport():
+    available_ports = list_ports.comports()
+    porta_escolhida = inquirer.prompt([
+        inquirer.List("porta", message="Escolha a porta serial", choices=[x.device for x in available_ports])
+    ])["porta"]
+    return porta_escolhida
