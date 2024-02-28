785f3e5f7da43b95f69dba9283063c8d741374e7
Bruno Gottardo Conti <bruno.conti@sou.inteli.edu.br>
2024-02-28 13:34:46 -0300

    att

--- src/main.py
+++ src/main.py
@@ -1,101 +1,50 @@
-# from robot import robot
-# import inquirer
-
-# robo = robot()
-
-# def execute_command(action, direction=None, distance=None):
-#     movements = {
-#         'x': lambda: robo.moveX(float(distance)),
-#         'y': lambda: robo.moveY(float(distance)),
-#         'z': lambda: robo.moveZ(float(distance)),
-#         'r': lambda: robo.moveR(float(distance))
-#     }
-
-#     commands = {
-#         "home": lambda: robo.moveHome(),
-#         "ligar": lambda: robo.actuatorOn(),
-#         "desligar": lambda: robo.actuatorOff(),
-#         "mover": lambda: movements[direction]() if direction in movements else print("Sentido inválido desconhecido."),
-#         "atual": lambda: robo.current(),
-#         "setHome": lambda: robo.setHome()
-#     }
-
-#     if action in commands:
-#         commands[action]()
-#     else:
-#         print("Comando desconhecido.")
-
-# def ask_for_command():
-#     questions = [
-#         inquirer.List('action',
-#                       message="Escolha um comando",
-#                       choices=['home', 'ligar', 'desligar', 'mover', 'atual', 'setHome']),
-#     ]
-#     answers = inquirer.prompt(questions)
-#     return answers['action']
-
-# def ask_for_movement_args():
-#     questions = [
-#         inquirer.List('direction', message="Qual direção?", choices=['x', 'y', 'z', 'r']),
-#         inquirer.Text('distance', message="Qual a distância?"),
-#     ]
-#     answers = inquirer.prompt(questions)
-#     return answers['direction'], answers['distance']
-
-# def main():
-#     while True:
-#         action = ask_for_command()
-#         if action == 'mover':
-#             direction, distance = ask_for_movement_args()
-#             execute_command(action, direction, distance)
-#         else:
-#             execute_command(action)
-
-# if __name__ == "__main__":
-#     main()
-
 from robot import Robot
 import inquirer
 
+# Initialize the Robot instance
 robo = Robot()
 
-def execute_command(action, direction=None, distance=None):
-    # Define movement functions for each direction
-    movements = {
-        'x': lambda: robo.moveX(float(distance)),
-        'y': lambda: robo.moveY(float(distance)),
-        'z': lambda: robo.moveZ(float(distance)),
-        'r': lambda: robo.moveR(float(distance))
-    }
-
-    # Define commands for home position, actuator control, and movements
-    commands = {
-        "home": lambda: robo.moveHome(),
-        "ligar": lambda: robo.actuatorOn(),
-        "desligar": lambda: robo.actuatorOff(),
-        "mover": lambda: movements[direction]() if direction in movements else print("Invalid direction."),
-        "atual": lambda: robo.current(),
-        "setHome": lambda: robo.setHome()
-    }
-
-    # Execute the given command
-    if action in commands:
-        commands[action]()
+def execute_command(action, direction=None, distance=None, filename=None):
+    if action == "mover":
+        movements = {
+            'x': lambda: robo.moveX(float(distance)),
+            'y': lambda: robo.moveY(float(distance)),
+            'z': lambda: robo.moveZ(float(distance)),
+            'r': lambda: robo.moveR(float(distance))
+        }
+        if direction in movements:
+            movements[direction]()
+        else:
+            print("Invalid direction.")
+    elif action == "moveToFilePosition":
+        if filename:
+            robo.moveToPositionFromFile(filename)
+        else:
+            print("No filename specified.")
+    elif action == "ligar":
+        robo.actuatorOn()
+    elif action == "desligar":
+        robo.actuatorOff()
+    elif action == "atual":
+        robo.current()
+    elif action == "savePosition":
+        if filename:
+            robo.setposition(filename)
+        else:
+            print("Filename not specified.")
     else:
         print("Unknown command.")
 
-def ask_for_command():
-    # Prompt the user to select a command
+def ask_for_action():
     questions = [
         inquirer.List('action',
                       message="Choose a command",
-                      choices=['home', 'ligar', 'desligar', 'mover', 'atual', 'setHome']),
+                      choices=['moveToFilePosition', 'ligar', 'desligar', 'mover', 'atual', 'savePosition']),
     ]
     answers = inquirer.prompt(questions)
     return answers['action']
 
 def ask_for_movement_args():
-    # Prompt the user for movement direction and distance if the action is 'mover'
     questions = [
         inquirer.List('direction', message="Which direction?", choices=['x', 'y', 'z', 'r']),
         inquirer.Text('distance', message="What distance?"),
@@ -103,12 +52,22 @@ def ask_for_movement_args():
     answers = inquirer.prompt(questions)
     return answers['direction'], answers['distance']
 
+def ask_for_filename():
+    question = [
+        inquirer.Text('filename', message="Enter filename"),
+    ]
+    answer = inquirer.prompt(question)
+    return answer['filename']
+
 def main():
     while True:
-        action = ask_for_command()
+        action = ask_for_action()
         if action == 'mover':
             direction, distance = ask_for_movement_args()
-            execute_command(action, direction, distance)
+            execute_command(action, direction=direction, distance=distance)
+        elif action in ['moveToFilePosition', 'savePosition']:
+            filename = ask_for_filename()
+            execute_command(action, filename=filename)
         else:
             execute_command(action)
 
--- src/robot.py
+++ src/robot.py
@@ -11,34 +11,44 @@ class Robot:
         self.x, self.y, self.z, self.r, self.j1, self.j2, self.j3, self.j4 = self.device.pose()
 
     def moveX(self, x):
+        self._update_pose()
         self.device.move_to(self.x + x, self.y, self.z, self.r, wait=True)
         self._update_pose()
         print("Moved X to:", self.x)
 
     def moveY(self, y):
+        self._update_pose()
         self.device.move_to(self.x, self.y + y, self.z, self.r, wait=True)
         self._update_pose()
         print("Moved Y to:", self.y)
 
     def moveZ(self, z):
+        self._update_pose()
         self.device.move_to(self.x, self.y, self.z + z, self.r, wait=True)
         self._update_pose()
         print("Moved Z to:", self.z)
 
     def moveR(self, r):
+        self._update_pose()
         self.device.move_to(self.x, self.y, self.z, self.r + r, wait=True)
         self._update_pose()
         print("Rotated R to:", self.r)
 
-    def moveHome(self):
-        json = dobotJson.readJson("home")
-        self.device.move_to(json['x'], json['y'], json['z'], json['r'], wait=True)
+    def moveToPositionFromFile(self, filename):
         self._update_pose()
-        print("Moved to home position")
+        json = dobotJson.readJson(filename)
+        if json:
+            self.device.move_to(json['x'], json['y'], json['z'], json['r'], wait=True)
+            self._update_pose()
+            print(f"Moved to position from file: {filename}.json")
+        else:
+            print(f"Failed to move: file {filename}.json not found.")
 
-    def setHome(self):
-        dobotJson.makeJson('home', self.x, self.y, self.z, self.r, self.j1, self.j2, self.j3, self.j4, "linear", "on")
-        print("Home position set")
+
+    def setposition(self, filename='home'):
+        self._update_pose()
+        dobotJson.makeJson(filename, self.x, self.y, self.z, self.r, self.j1, self.j2, self.j3, self.j4, "linear", "off")
+        print(f"Position saved as {filename}")
 
     def actuatorOn(self):
         self.device.suck(True)
@@ -49,4 +59,5 @@ class Robot:
         print("Actuator turned off")
 
     def current(self):
+        self._update_pose()  # Make sure this is called before printing
         print(f"Current Position: x={self.x}, y={self.y}, z={self.z}, r={self.r}")
